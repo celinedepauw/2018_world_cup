18b776574dfea49a3cc0b92ed229a75abb574346
celin <celin@localhost>
2022-01-11 10:10:04 +0100

    integrate reactive form in login component

--- 2018_world_cup/src/app/app.module.ts
+++ 2018_world_cup/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule} from '@angular/flex-layout';
-
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { GroupComponent } from './group/group.component';
@@ -34,7 +32,8 @@ import { TeamComponent } from './team/team.component';
     FlexLayoutModule,
     MatMenuModule,
     MatCardModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 2018_world_cup/src/app/login/login.component.ts
+++ 2018_world_cup/src/app/login/login.component.ts
@@ -1,7 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
+// UTILISATION REACTIVE FORM
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+email = new FormControl('');
+password = new FormControl('');
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit() {
+    /*if (this.email !== 'nacer@devid.com') {
+      alert('email incorrect')
+    } else if (this.password !== 'FutbalL2018') {
+      alert('mot de passe incorrect')
+    } else {
+      this.router.navigateByUrl('/home');
+    }*/
+  }
+}
+
+/* VALIDATION FORMULAIRE BASIQUE
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -29,4 +60,4 @@ password: string = '';
       this.router.navigateByUrl('/home');
     }
   }
-}
+}*/
